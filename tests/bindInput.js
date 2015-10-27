1d4dbb89ce7cae6cade110bc8554cecdf675f75c
Goran Košutić <gkosutic@tofex.de>
2015-10-27 14:57:38 +0100

    write tests with arrow functions

--- tests/bindInput.js
+++ tests/bindInput.js
@@ -1,12 +1,12 @@
 import $ from 'jquery';
 import bindInput from '../source/bindInput';
 
-QUnit.test( 'should be defined', function(assert) {
-    
-    assert.ok( bindInput, 'plugin does not exist' );
-});
+QUnit.test(
+    'should be defined',
+    assert => assert.ok( bindInput, 'plugin does not exist' )
+);
 
-QUnit.test( 'should be added as a jQuery plugin', function(assert) {
-    
-    assert.ok( $.fn.bindInput, 'plugin not added to jQuery' );
-});
+QUnit.test(
+    'should be added as a jQuery plugin',
+    assert => assert.ok( $.fn.bindInput, 'plugin not added to jQuery' )
+);
